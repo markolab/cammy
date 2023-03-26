3e00faf702663492ca534507c62dc177b4d06871
Jeffrey Markowitz <markowitzmeister@gmail.com>
2023-03-26 15:05:12 -0400

    Auto select serial port if one available

--- cammy/trigger/trigger.py
+++ cammy/trigger/trigger.py
@@ -41,16 +41,20 @@ class TriggerDevice:
 def select_serial_port():
     all_ports = serial.tools.list_ports.comports()
 
-    print("Select serial port to connect to microcontroller:")
-    print("-" * 10)
-    for idx, port in enumerate(all_ports):
-        print("[{}] {}".format(idx, port.device))
-    print("-" * 10)
+    if len(all_ports) > 1:
+        print("Select serial port to connect to microcontroller:")
+        print("-" * 10)
+        for idx, port in enumerate(all_ports):
+            print("[{}] {}".format(idx, port.device))
+        print("-" * 10)
 
-    selection = None
-    while selection is None:
-        selection = click.prompt("Enter a selection", type=int)
-        if selection > len(all_ports) or selection < 0:
-            selection = None
-
+        selection = None
+        while selection is None:
+            selection = click.prompt("Enter a selection", type=int)
+            if selection > len(all_ports) or selection < 0:
+                selection = None
+    else:
+        selection = 0
+        print("Using: {}".format(all_ports[0].device))
+ 
     return all_ports[selection].device
