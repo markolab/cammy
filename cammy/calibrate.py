11aafcacc7d82a1050b3648838998a07f3cb0466
Jeffrey Markowitz <markowitzmeister@gmail.com>
2023-09-18 22:47:27 -0400

    Bugfix

--- cammy/calibrate.py
+++ cammy/calibrate.py
@@ -18,7 +18,7 @@ def initialize_boards(
     for slc in tqdm(range(num_slices)):
         aruco_idxs = (np.arange(left_edge, left_edge + markers_per_slice),)
         boards.append(
-            cv2.aruco.CharucoBoard_create(
+            cv2.aruco.CharucoBoard(
                 squares, square_length, marker_length, aruco_dict, aruco_idxs
             )
         )
